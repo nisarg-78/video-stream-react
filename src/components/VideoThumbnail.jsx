892cfef1f6988eda4b56f63cca80587f2373166a
nisarg-78 <gajjarnisarg78@gmail.com>
2023-12-31 20:27:24 +0530

    ui changes

--- src/components/VideoThumbnail.jsx
+++ src/components/VideoThumbnail.jsx
@@ -3,15 +3,21 @@ import styles from "./VideoThumbnail.module.css"
 import { Link } from "react-router-dom"
 
 const VideoThumbnail = ({ img, title, id }) => {
-
 	return (
 		<div className={styles.video}>
-			<div className={styles.thumbnail}>
 			<Link to={`/${id}`}>
-				<img src={img} alt='' className="thumbnail" />
+				<div className={styles.thumbnail}>
+					<img
+						src={"https://source.unsplash.com/random"}
+						alt=''
+						className='thumbnail'
+					/>
+				</div>
 			</Link>
+			<div className={styles.title} title={title}>
+				{title.substring(0, 27)}
+				{title.length > 30 ? "..." : ""}
 			</div>
-			<div className={styles.title}>{title}</div>
 		</div>
 	)
 }
